3567a4d92eb75ee092c4af740f26d7981ee09d72
JohnsonChung <johnson780510@gmail.com>
2016-04-29 00:13:50 +0800

    basic content layout

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,36 @@
-/**
- * Created by johns on 2016/4/28.
- */
+$(document)
+    .ready(function () {
+
+        // fix menu when passed
+        $('.masthead')
+            .visibility({
+                once: false,
+                onBottomPassed: function () {
+                    $('.fixed.menu').transition('fade in');
+                },
+                onBottomPassedReverse: function () {
+                    $('.fixed.menu').transition('fade out');
+                }
+            });
+
+        // create sidebar and attach to menu open
+        $('.ui.sidebar')
+            .sidebar('attach events', '.toc.item');
+
+
+        var bgCanvas = document.getElementById('#masthead');
+        bgCanvas.patternizer({
+            stripes : [
+                {
+                    color: '#ffffff',
+                    rotation: 45,
+                    opacity: 21,
+                    mode: 'plaid',
+                    width: 1,
+                    gap: 185,
+                    offset: 0
+                }
+            ],
+            bg : '#000'
+        });
+});
