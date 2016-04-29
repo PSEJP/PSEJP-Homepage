cfd23ab991f1907d05b2a3a1aec1078cb033fb6f
JohnsonChung <johnson780510@gmail.com>
2016-04-30 02:08:10 +0800

    layout simplify and improvement

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,20 +1,15 @@
 $(document)
     .ready(function () {
 
-        // fix menu when passed
-        $('.masthead')
-            .visibility({
-                once: false,
-                onBottomPassed: function () {
-                    $('.fixed.menu').transition('fade in');
-                },
-                onBottomPassedReverse: function () {
-                    $('.fixed.menu').transition('fade out');
-                }
-            });
-
         // create sidebar and attach to menu open
         $('.ui.sidebar')
-            .sidebar('attach events', '.toc.item');
+            .sidebar('attach events', '.toc.item.toggle');
 
-});
+        // click anchor toggle body unresolved class
+        $('.ui.large.secondary.inverted.pointing.menu > a:not(.toggle)').bind('click', function (e) {
+            //e.preventDefault();
+            if ( e.target.href === '#' || e.target.href === 'null' || e.target.href === 'undefined'  ) return;
+            $('body').addClass('unresolved');
+        });
+
+    });
