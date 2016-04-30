be2428cfcaefdcea23dccc7de6d6764a4e838762
touhonoob <touhonoob@gmail.com>
2016-04-30 18:22:51 +0800

    add build script

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var recruit = require('./routes/recruit');
 var support = require('./routes/support');
 
 var app = express();
+app.locals.env = app.settings.env;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,6 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/index', routes);
 app.use('/about', about);
 app.use('/develop', develop);
 app.use('/web-management', webManage);
---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+var fork = require('child_process').fork;
+var path = require('path');
+var request = require('request');
+var fs = require('fs');
+var async = require('async');
+
+gulp.task('default', () => {
+    const PORT = 3000;
+    const DIST = path.join(__dirname, 'public');
+
+    const serverProcess = fork(path.join('bin', 'www'), {
+        env: {
+            NODE_ENV: 'production'
+        }
+    });
+
+    serverProcess.on('message', (message) => {
+        if (message.success) {
+            build();
+        } else {
+            console.error('Express Failed');
+        }
+    });
+
+    function build() {
+        var pages = {
+            '/index': 'index.html',
+            '/about': 'about.html',
+            '/develop': 'develop.html',
+            '/web-management': 'web-management.html',
+            '/online-support': 'online-support.html',
+            '/recruit': 'recruit.html',
+            '/support': 'support.html',
+            '/404': '404.html'
+        };
+
+        async.every(Object.keys(pages), (route, callback) => {
+            const file = pages[route];
+            const stream = request
+                .get(`http://localhost:${PORT}${route}`)
+                .on('error', (err) => {
+                    console.error(err)
+                })
+                .pipe(fs.createWriteStream(path.join(DIST, file)));
+
+            stream.on('finish', () => {
+                callback(null, true);
+            });
+        }, () => {
+            serverProcess.kill('SIGINT')
+        });
+    }
+});
